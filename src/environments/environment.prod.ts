b1ef51b671c35e368381c5b43fd1c61c709efaa1
alejandrofm98 <alejandrofmayor98@gmail.com>
2026-02-07 21:33:51 +0100

    chore: Remover variables de entorno API_WALACTV y ACE_STREAM_HOST
    Se eliminan los ARGs y la inyección de estas variables en los
    archivos de entorno de Dockerfiles, ya que no son utilizadas
    actualmente en la configuración del proyecto.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  apiWalactv: '${API_WALACTV}',
-  acestreamHost: '${ACE_STREAM_HOST}',
   appName: '${APP_NAME}',
   iptvApiUrl: '${IPTV_API_URL}'
 };
